82b366cfd285b43df7bb1fd4b98e761d4c4592b0
vladexologija <vladimir@p3rsonal.com>
2012-11-21 17:22:50 +0100

    blog

---
+++ trunk/blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '01ea460e1f5662242c1e5ff967780e413c7c16d45853c3cfc4303f18fbdb99248db50fd015db5e6ef170a15379f06de5df146966897f5e223545af6106fd06f0'
