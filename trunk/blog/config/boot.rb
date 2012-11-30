44e4dd5c95a645392ddd48e1a36f7fa950ba60ee
vladexologija <vladimir@p3rsonal.com>
2012-11-30 17:44:05 +0100

    db

--- trunk/blog/config/boot.rb
+++ trunk/blog/config/boot.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 
 # Set up gems listed in the Gemfile.
+# Ruby idiom for getting the absolute path to a file when you know the path relative to the current file
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
