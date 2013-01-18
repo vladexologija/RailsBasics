558fb804c947a7cdeac386df2a7057448f187adb
vladexologija <vladimir@p3rsonal.com>
2013-01-18 10:01:47 +0100

    page caching

---
+++ trunk/blog/app/sweepers/post_sweeper.rb
@@ -0,0 +1,15 @@
+class PostSweeper < ActionController::Caching::Sweeper
+  observe Post
+
+  def sweep(post)
+    expire_page posts_path
+    expire_page post_path(post)
+    expire_page root_path
+    FileUtils.rm_rf "#{page_cache_directory}/posts/page"
+  end
+
+  alias_method :after_update, :sweep
+  alias_method :after_create, :sweep
+  alias_method :after_destroy, :sweep
+
+end
