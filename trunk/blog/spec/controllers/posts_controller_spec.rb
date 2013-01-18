de1167eed196120cdce6e6ece97aa409e570d8fe
vladexologija <vladimir@p3rsonal.com>
2013-01-18 18:02:46 +0100

    rspec

---
+++ trunk/blog/spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,6 @@
+require "spec_helper"
+
+describe "Create Post" do
+  let (:post) { Factory(:post) }
+
+end
--- trunk/blog/spec/models/post_spec.rb
+++ trunk/blog/spec/models/post_spec.rb
@@ -1,14 +1,34 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Post do
+  let(:post) { Factory(:timesheet) }
+
 
   it "description should be 'n/a' if not available" do
-    post = Post.new
     post.description.should == "n/a"
   end
 
-  it "description should return category when available" do
-    post = Post.new({:description => "something"})
+  it "description should return something when available" do
     post.description.should == "something"
+  end
+
+  describe "validation of description worked" do
+    it "fails if description is shorter than 5 characters" do
+      post.description = "two"
+      post.should have(1).error_on(:description)
+    end
+
+    it "passes with longer description" do
+      post.description = "description"
+      post.should have(0).error_on(:description)
+    end
+  end
+
+  context "when submited" do
+
+    it "updates the time" do
+      post.created_at.should_not be_nil
+      post.submit
+    end
   end
 end
